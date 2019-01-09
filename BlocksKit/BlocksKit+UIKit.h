af31be18b33695359324aebd8f430eda314b7f07
Hiroyuki Koike <hiroyuki.koike@piascore.com>
2019-01-09 18:47:01 +0900

    Modify

--- BlocksKit/BlocksKit+UIKit.h
+++ BlocksKit/BlocksKit+UIKit.h
@@ -34,7 +34,7 @@
 #import <BlocksKit/UIGestureRecognizer+BlocksKit.h>
 #import <BlocksKit/UIImage+BlocksKit.h>
 #import <BlocksKit/UIImagePickerController+BlocksKit.h>
-#import <BlocksKit/UIPopoverController+BlocksKit.h>
+//#import <BlocksKit/UIPopoverController+BlocksKit.h>
 #import <BlocksKit/UITextField+BlocksKit.h>
 #import <BlocksKit/UIView+BlocksKit.h>
 #import <BlocksKit/UIWebView+BlocksKit.h>
